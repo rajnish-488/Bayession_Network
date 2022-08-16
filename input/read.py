e6805645e7a6d93a0b53c0ec4d15ebdff888cbf7
rajnish-pracbee <rajnish.singh@pracbee.com>
2022-08-16 09:44:18 +0530

    ***

--- input/read.py
+++ input/read.py
@@ -4,5 +4,9 @@ import numpy as np
 class read_data:
     def __init__(self,path):
         self.path=path
+        self.df=None
+    
+    def read(self):
+        self.df=pd.read_csv(self.path)
 
-    def
+    
