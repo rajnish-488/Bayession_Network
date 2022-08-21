5ca3df5d818846842587c2767deed8799c55b9e2
rajnish-pracbee <rajnish.singh@pracbee.com>
2022-08-22 01:31:32 +0530

    add the following add realtion

--- CPP/Baysseion_Network/rajnish.h
+++ CPP/Baysseion_Network/rajnish.h
@@ -66,6 +66,24 @@ class Bayesian_Network{
 
 
 	//based on given relation find the relation and put in each nodes parent and children (Akash FuckBoy)
+	void add_relation(){
+        string ancestor, child;
+
+        for(int j=0; j<relations.size(); j++){
+            ancestor=relations[j].first, child=relations[j].second;
+
+            for(int i=0; i<model->arr.size(); i++){
+                
+                if( model->arr[i]->columnName == (child)) {
+                    model->arr[i]->parent.push_back(ancestor);
+                }
+
+                if( model->arr[i]->columnName == (ancestor) ) {
+                    model->arr[i]->next.push_back(child);
+                }
+            }
+        }
+    }
 
 
 	// check the relation { Rajnish }
@@ -77,7 +95,7 @@ class Bayesian_Network{
 	vector<pair<string,vector<string>>> solve(vector<Node*> parent,int i,vector<string> &v,int n,Node* A){
 		if(i==n){
 			for(int i=0;i<n;i++){
-				
+
 			}
 		}
 	}
