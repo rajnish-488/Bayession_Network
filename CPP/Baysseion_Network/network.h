a73d311a88ccbac55be2abaa34ff77d812c9eeed
rajnish-pracbee <rajnish.singh@pracbee.com>
2022-11-03 20:04:17 +0530

    the detection of cycle

--- CPP/Baysseion_Network/network.h
+++ CPP/Baysseion_Network/network.h
@@ -111,6 +111,66 @@ class Bayesian_Network{
 		}
 	}
 
+	bool checkCycle(int node, vector < int > adj[], int vis[], int dfsVis[]) {
+      vis[node] = 1;
+      dfsVis[node] = 1;
+      for (auto it: adj[node]) {
+        if (!vis[it]) {
+          if (checkCycle(it, adj, vis, dfsVis)) return true;
+        } else if (dfsVis[it]) {
+          return true;
+        }
+      }
+      dfsVis[node] = 0;
+      return false;
+    }
+
+
+	bool isCyclic(int N, vector < int > adj[]) {
+      int vis[N], dfsVis[N];
+     
+      for(int i = 0; i < N; i++){
+        vis[i] = 0;
+        dfsVis[i] = 0;
+      }
+
+      for (int i = 0; i < N; i++) {
+        if (!vis[i]) {
+          // cout << i << endl; 
+          if (checkCycle(i, adj, vis, dfsVis)) {
+             // cout << i << endl;
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+	void addEdge(vector < int > adj[], int u, int v) {
+		adj[u].push_back(v);
+	}
+
+	string isDAG(){
+		map<string,int> mp;
+		int i=0;
+		int V=data.size();
+		vector < int > adj[V];
+		for(auto x: data){mp[x.first]=i;i++;}
+		for(auto x: relations){
+			if(mp.find(x.first)!=mp.end() and mp.find(x.second)!=mp.end())
+				addEdge(adj, mp[x.first], mp[x.second]);
+			else
+				return "The entered Relations can't form a garph.";
+		}
+		if (isCyclic(V, adj))
+			return "Cycle Detected";
+		else
+			return "No Cycle Detected";
+
+		return "Some Error";
+
+	}
+
 	void build_condition(Node* root,vector<vector<string>> &arr,int sz,string ans){
 		if(sz==arr.size()){
 			root->condition.push_back(ans);
@@ -225,161 +285,10 @@ class Bayesian_Network{
 		}
 	}
 
-	// // check the relation { Rajnish }
-	// bool checkCycle(int node, unordered_map<string, list<string> > &adj, unordered_map<string , bool >& visited , unordered_map<string , bool > & dfsvisited) {
-	// 	visited[node] = true;
-	// 	dfsvisited[node] = true;
 
-	// 	for(auto neighbour: adj[node]){
-	// 		if(visited[neighbour]==false){
-	// 			bool cycleDetected = checkCycle(neighnour,adj,visited,dfsvisited);
-	// 			if(cycleDetected)
-	// 				return true;
-	// 		}
-	// 		else if(dfsvisited[neighbour]){
-	// 			return true
-	// 		}
-	// 	}
-	// 	dfsvisited[node] = false;
-	// 	return false;
-	// }
+	vector<string> predict(map<string,string> mp){
 
-
-	// // taking no of nodes or vertices 
-	// bool isDAG()
-	// {
-	// 	// No. of nodes or vertices
-	// 	int vertices=data.size();
-
-	// 	// create a adjacency list for all nodes
-	// 	unordered_map<string, list<string> > adj;
-
-
-	// 	for(int i=0;i<relations.size();i++){
-	// 		int u = relations[i].first;
-	// 		int v = relations[i].second;
-
-	// 		adj[u].push_back(v);
-	// 	}
-
-	// 	// make a visited and dfsvisited map 
-	// 	unordered_map<string , bool > visited;
-	// 	unordered_map<string , bool > dfsvisited;
-		
-	// 	for(int i = 1; i <=vertices; i++)
-	// 	{
-	// 		if(visited[i]==false){
-	// 			// call checkcycle function for detect DAG in a given relation 
-	// 			if(checkCycle(i, adj, visited, dfsvisited)) {
-	// 				return true;
-	// 			}
-	// 		}
-	// 	}
-	// 	return false;
-	// }
-
-
-	// //filling the conidtion table based on the parent node;
-	// vector<vector<int>> build(vector<vector<int>> & arr, int type){
-    //     vector<vector<int>> ar;
-    //     if(arr.size()==0){
-    //         for(int i=0; i<type; i++){
-    //             vector<int> a = {i};
-    //             ar.push_back(a);
-    //         }
-    //     }
-    //     else{
-    //         for(int j=0; j<type; j++){
-    //             for(int i=0; i<arr.size(); i++){
-    //                 vector<int> temp = arr[i];
-    //                 temp.push_back(j);
-    //                 ar[i].push_back(temp);
-    //             }
-    //         }
-    //     }
-
-    //     return ar;
-    // }
-
-    // vector<pair<string, vector<string>>> create_condition_table(vector<Node*> parent, int i, vector<string> &v, int n, Node* A){
-	// 	vector<vector<int>> table;
-        
-    //     for(int i=0; i<parent.size(); i++){
-    //         int val = parent->type.size();
-    //         v.push_back(parent->columnName);
-    //         build(table, val);
-    //     }
-        
-	// }
-
-	// // based on condition we havew to create the probabolity;
-	// void setProbability(Node * root){
-	// 	//initiate the probability datatype;
-	// 	for(string x: root->type){
-	// 		vector<double> vv;
-	// 		root->probability.push_back({x,vv});
-	// 	}
-		
-	// 	int m=root->condition.size();
-
-	// 	if(m!=0){
-	// 		int n=root->condition[0].second.size();
-	// 		vector<pair<string,string>> pp; // to store all the values of the in each row
-	// 		for(int i=0;i<m;i++){// to store the column name in pair;
-	// 			pp.push_back({root->condition[i].first,""});
-	// 		}
-	// 		for(int i=0;i<n;i++){// travesed to get the values of each row;
-	// 			for(int j=0;j<m;j++){
-	// 				// setting all the values of each pair in the condition row;
-	// 				pp[j].second=root->condition[j].second[i];
-	// 			}
-	// 			vector<int> arr;//vector<to store all the values;
-	// 			int nn=data[root->columnName].size();
-	// 			bool check;
-	// 			for(int j=0;j<nn;j++){
-	// 				// travese therought all the values of data;
-	// 				check=true;
-	// 				for(int k=0;k<m;k++){
-	// 					//traverse frrom all the values in column of pp vector<pair>
-	// 					if(data[pp[k].first][j]!=pp[k].second){
-	// 						check=false;// checking if the pp data is same for that row in data
-	// 						break
-	// 					}
-	// 				}
-
-	// 				if(check){// it the condition that the column is the one which is the  solution
-	// 					arr.push_back(data[root->columnName][j]);
-	// 				}
-	// 				int ppp=root->probability.size();
-	// 				int arr_n=arr.size()
-
-	// 				for(int k=0;k<ppp;k++){//pass through all the values of the probability
-	// 					int data=count(arr,arr + arr_n,root->probability[k].first);
-	// 					double xx=data/arr_n;
-	// 					if(xx==0) x=1e-6;
-	// 					root->probability[k].second.push_back(xx);
-	// 				}
-
-
-	// 			}
-
-	// 		}
-
-
-	// 	}
-	// }
-
-	// void findProbanbility(){
-	// 	int n=mode->arr.size();
-	// 	for(int i=0;i<n;i++){
-	// 		setProbability(model->arr[i]);
-	// 	}
-
-	// }
-
-
-
-	//got to all nodes and find the probability and multuiply it;
+	}
 
 };
 
